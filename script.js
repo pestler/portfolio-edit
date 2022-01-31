2d3ffb39b9b9e5f7190822f6a6417972f483fba0
Aleh <oleh@tut.by>
2022-01-31 14:47:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,28 @@
-console.log('(1) Вёрстка валидная (+10)\n',
-    '(2) Вёрстка семантическая (+20)\n',
-    '    header, main, footer (+2)\n',
-    '    шесть элементов <section> (по количеству секций) (+2)\n',
-    '    только один заголовок h1 (+2)\n',
-    '    пять заголовков h2 (+2)\n',
-    '    один элемент <nav> (панель навигации) (+2)\n',
-    '    два списка ul > li > a (панель навигации, ссылки на соцсети) (+2)\n',
-    '    десять кнопок <button> (+2)\n',
-    '    два инпута: <input type="email"> и <input type="tel"> (+2)\n',
-    '    один элемент <textarea> (+2)\n',
-    '    три атрибута placeholder (+2)\n',
-    '(3) Вёрстка соответствует макету (+48)\n',
-    '    блок <header> (+6)\n',
-    '    секция hero (+6)\n',
-    '    секция skills (+6)\n',
-    '    секция portfolio (+6)\n',
-    '    секция video (+6)\n',
-    '    секция price (+6)\n',
-    '    секция contacts (+6)\n',
-    '    блок <footer> (+6)\n',
-    '(4) Требования к css (+12)\n',
-    '    для построения сетки используются флексы или гриды (+2)\n',
-    '    при уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону (+2)\n',
-    '    фоновый цвет тянется на всю ширину страницы (+2)\n',
-    '    иконки добавлены в формате .svg (+2)\n',
-    '    изображения добавлены в формате .jpg (+2)\n',
-    '    есть favicon (+2)\n',
-    '(5) Интерактивность, реализуемая через css (+20)\n',
-    '    плавная прокрутка по якорям (+5)\n',
-    '    ссылки в футере ведут на гитхаб автора проекта и на страницу курса https://rs.school/js-stage0/ (+5)\n',
-    '    интерактивность включает в себя не только изменение внешнего вида курсора (+5)\n',
-    '    плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы (+5)\n',
-    'Score: 100\n',
-)
-
+import i18Obj from "./translate.js";
 
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-active');
 const nav = document.querySelector('.nav-active');
 
-
 function toggleMenu() {
     hamburger.classList.toggle('open');
-    nav.classList.toggle('open')
+    nav.classList.toggle('open');
 }
 hamburger.addEventListener('click', toggleMenu);
 
+/* change images*/
+const portfolioBtns = document.querySelectorAll('.portfolio-btn')
+const imgPortfolio = document.querySelectorAll('.portfolio-img')
+function changeImages() {
+    portfolioBtns.forEach(element => element.addEventListener('click', () => {
+        portfolioBtns.forEach(element => element.classList.remove('active'));
+        imgPortfolio.forEach((el, index) => {
+            el.src = `assets/img/${element.dataset.season}/${index + 1}.jpg`
+        })
+        element.classList.add('active');
+    }))
+}
+ changeImages();
 
-
-
-
-
-
+ /*translate */
+ 
